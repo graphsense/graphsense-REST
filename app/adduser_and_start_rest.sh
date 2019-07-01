48922151b83179f59bd45669f0188943cb0453a1
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2019-07-01 15:22:37 +0200

    Fix shellcheck warnings

--- app/adduser_and_start_rest.sh
+++ app/adduser_and_start_rest.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 sudo mkdir /var/lib/graphsense-rest;
 sudo chmod 777 /var/lib/graphsense-rest;
-python3 adddefaultusers.py $1 $2;
+python3 add_default_users.py "$1" "$2";
 python3 graphsenserest.py;
