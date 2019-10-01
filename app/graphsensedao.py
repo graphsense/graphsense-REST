459b7f74165f883737fa9d434ea6455c92bf4615
Matteo Romiti <matt.7.19.93@gmail.com>
2019-10-01 16:55:00 +0200

    remove debug line for real

--- app/graphsensedao.py
+++ app/graphsensedao.py
@@ -157,7 +157,6 @@ def query_address(currency, address):
 def query_address_cluster(currency, address):
     set_keyspace(session, currency)
     clusterid = query_address_cluster_id(currency, address)
-    print('+++++++', clusterid, '+++++++')
     ret = {}
     if clusterid:
         cluster_obj = query_cluster(currency, clusterid)
