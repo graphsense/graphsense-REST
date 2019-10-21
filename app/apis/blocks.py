cb1e9c041a394266ce0442439592f767361c084c
Bernhard Haslhofer <bernhard.haslhofer@ait.ac.at>
2019-10-21 22:38:55 +0200

    implemented auth decorators

--- app/apis/blocks.py
+++ app/apis/blocks.py
@@ -1,5 +1,7 @@
 from flask_restplus import Namespace, Resource
 
+from app.util.decorator import token_required
+
 api = Namespace('blocks',
                 path='/<currency>/blocks',
                 description='Operations related to blocks')
@@ -7,17 +9,20 @@ api = Namespace('blocks',
 
 @api.route("/")
 class Blocks(Resource):
+    @token_required
     def get(self, currency):
         return "NOT YET IMPLEMENTED"
 
 
 @api.route("/<int:height>")
 class Block(Resource):
+    @token_required
     def get(self, currency, height):
         return "Requested currency {}, block {}".format(currency, height)
 
 
 @api.route("/<int:height>/transactions")
 class BlockTransactions(Resource):
+    @token_required
     def get(self, currency, height):
         return "NOT YET IMPLEMENTED"
--- app/service/auth_helper.py
+++ app/service/auth_helper.py
@@ -59,3 +59,30 @@ class Auth:
                 'message': 'Provide a valid auth token.'
             }
             return response_object, 403
+
+    @staticmethod
+    def get_logged_in_user(new_request):
+            # get the auth token
+            auth_token = new_request.headers.get('Authorization')
+            if auth_token:
+                resp = User.decode_auth_token(auth_token)
+                user = find_user(resp)
+                if user:
+                    response_object = {
+                        'status': 'success',
+                        'data': {
+                            'user_id': user.username
+                        }
+                    }
+                    return response_object, 200
+                response_object = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return response_object, 401
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': 'Provide a valid auth token.'
+                }
+                return response_object, 401
