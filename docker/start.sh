c2605bcb9bb559be31d6319281224a2c89adc845
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2019-11-20 13:48:38 +0100

    Add --cap-drop to docker run command

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker stop graphsense-rest
 docker rm graphsense-rest
-docker run --restart=always -d --name graphsense-rest -p 9000:9000 -it graphsense-rest
+docker run --restart=always -d --name graphsense-rest \
+    --cap-drop all \
+    -p 9000:9000 \
+    -it graphsense-rest 
 docker ps -a
