71f0dae5ea5a46c10cd6288e2d8aadaf98e570f4
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2020-01-13 16:19:15 +0100

    Improve docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+if [[ -f "instance/config.py" ]]; then
+    echo "Error: config file 'instance/config.py' does not exist."
+fi
+
 docker build -t graphsense-rest --build-arg NUM_WORKERS=3 .
