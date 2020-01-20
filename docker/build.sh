1f4bf087501cb3e8817e39cac14ad1f8e4e5f980
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2020-01-20 09:53:05 +0100

    Fix Docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ -f "instance/config.py" ]]; then
+if [[ ! -f "instance/config.py" ]]; then
     echo "Error: config file 'instance/config.py' does not exist."
+    exit 1
 fi
 
 docker build -t graphsense-rest --build-arg NUM_WORKERS=3 .
