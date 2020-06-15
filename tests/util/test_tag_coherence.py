15349b1c0049e401150107947a1827b642b09046
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2020-06-15 20:35:05 +0200

    Fix pylint warnings

--- tests/util/test_tag_coherence.py
+++ tests/util/test_tag_coherence.py
@@ -1,43 +1,42 @@
 from gsrest.util.tag_coherence import compute_tag_coherence
 
 
-def test_compute_tag_coherence(client, auth, monkeypatch):
+def test_compute_tag_coherence():
     tags = []
     assert compute_tag_coherence(tags) is None
 
-    def toTags(labels):
+    def to_tags(labels):
         return [{'label': label} for label in labels]
 
-    tags = toTags(['foo'])
+    tags = to_tags(['foo'])
     assert compute_tag_coherence(tags) == 1
 
-    tags = toTags(['foo', 'foo'])
+    tags = to_tags(['foo', 'foo'])
     assert compute_tag_coherence(tags) == 1
 
-    tags = toTags(['foo', 'foo', 'foo'])
+    tags = to_tags(['foo', 'foo', 'foo'])
     assert compute_tag_coherence(tags) == 1
 
-    tags = toTags(['foo', 'bar'])
+    tags = to_tags(['foo', 'bar'])
     assert compute_tag_coherence(tags) == 0
 
-    tags = toTags(['foo', 'bar', '42'])
+    tags = to_tags(['foo', 'bar', '42'])
     assert compute_tag_coherence(tags) == 0
 
-    tags = toTags(['foo', 'foobar'])
+    tags = to_tags(['foo', 'foobar'])
     assert round(compute_tag_coherence(tags), 3) == 0.667
 
-    tags = toTags(['tide', 'diet'])
+    tags = to_tags(['tide', 'diet'])
     assert round(compute_tag_coherence(tags), 3) == 0.375
 
-    tags = toTags(['foo', 'foo', 'bar'])
+    tags = to_tags(['foo', 'foo', 'bar'])
     assert compute_tag_coherence(tags) == 0.5
 
-    tags = toTags(['foo', 'foo', 'foo', 'bar'])
+    tags = to_tags(['foo', 'foo', 'foo', 'bar'])
     assert compute_tag_coherence(tags) == 0.75
 
-    tags = toTags(['foo', 'foo', 'foo', 'foo', 'bar'])
+    tags = to_tags(['foo', 'foo', 'foo', 'foo', 'bar'])
     assert round(compute_tag_coherence(tags), 3) == 0.857
 
-    tags = toTags(['tide', 'tide', 'diet'])
-    assert round(compute_tag_coherence(tags), 3) \
-        == 0.688
+    tags = to_tags(['tide', 'tide', 'diet'])
+    assert round(compute_tag_coherence(tags), 3) == 0.688
