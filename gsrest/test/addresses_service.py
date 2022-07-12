59492bd538dc3f3cb1241d88fc74edae8c5e9756
Matthias <git@myrho.net>
2022-07-12 19:27:11 +0200

    fix list_address_txs

--- gsrest/test/addresses_service.py
+++ gsrest/test/addresses_service.py
@@ -654,18 +654,21 @@ async def list_address_txs(test_case):
                     address_txs=[
                         AddressTxUtxo(
                             tx_hash="123456",
+                            currency="btc",
                             value=convert_value('btc', 1260000, rates[2]),
                             height=2,
                             coinbase=False,
                             timestamp=1510347493),
                         AddressTxUtxo(
                             tx_hash="abcdef",
+                            currency="btc",
                             value=convert_value('btc', -1260000, rates[2]),
                             height=2,
                             coinbase=False,
                             timestamp=1511153263),
                         AddressTxUtxo(
                             tx_hash="4567",
+                            currency="btc",
                             value=convert_value('btc', -1, rates[2]),
                             height=2,
                             coinbase=False,
