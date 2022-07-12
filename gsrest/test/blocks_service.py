71ddf1207a886263002dbf3397ead47925014422
Matthias <git@myrho.net>
2022-07-12 19:24:10 +0200

    fix get_block

--- gsrest/test/blocks_service.py
+++ gsrest/test/blocks_service.py
@@ -3,6 +3,7 @@ from gsrest.test.txs_service import tx1, tx1_eth, tx2_eth
 
 block = Block(
         height=1,
+        currency='btc',
         block_hash="00000000839a8e6886ab5951d76f"
         "411475428afc90947ee320161bbf18eb6048",
         no_txs=1,
@@ -10,6 +11,7 @@ block = Block(
 
 block2 = Block(
         height=2,
+        currency='btc',
         block_hash="000000006a625f06636b8bb6ac7b9"
         "60a8d03705d1ace08b1a19da3fdcc99ddbd",
         no_txs=1,
@@ -17,12 +19,14 @@ block2 = Block(
 
 eth_block = Block(
         height=1,
+        currency='eth',
         block_hash="123456",
         no_txs=5,
         timestamp=123)
 
 eth_block2 = Block(
         height=2300001,
+        currency='eth',
         block_hash="234567",
         no_txs=0,
         timestamp=234)
