7ed9e420babdb0a20e181766f54afead0887db23
Michael F <michael.froewis@gmx.at>
2023-01-13 13:25:45 +0100

    return pseudo rates for stable coin tokens

--- gsrest/service/rates_service.py
+++ gsrest/service/rates_service.py
@@ -1,5 +1,6 @@
 from openapi_server.models.rates import Rates
 from gsrest.service.stats_service import get_no_blocks
+from gsrest.util.values import map_rates_for_peged_tokens
 
 RATES_TABLE = 'exchange_rates'
 
@@ -14,7 +15,15 @@ async def get_rates(request, currency, height=None):
         height = (await get_no_blocks(request, currency)) - 1
 
     db = request.app['db']
-    r = await db.get_rates(currency, height)
+
+    eth_token_config = db.get_token_configuration("eth")
+    if currency.upper() in eth_token_config:
+        # create pseudo rates for eth stable coin tokens.
+        r = await db.get_rates("eth", height)
+        r["rates"] = map_rates_for_peged_tokens(
+            r['rates'], eth_token_config[currency.upper()])
+    else:
+        r = await db.get_rates(currency, height)
 
     if r is None:
         raise ValueError("Cannot find height {} in currency {}".format(
--- gsrest/test/rates_service.py
+++ gsrest/test/rates_service.py
@@ -9,7 +9,34 @@ rate = Rates(height=1,
                  'value': 0.0
              }])
 
+rate_eth = Rates(height=1,
+                 rates=[{
+                     'code': 'eur',
+                     'value': 1.0
+                 }, {
+                     'code': 'usd',
+                     'value': 2.0
+                 }])
+
+rate_usdstable_coin = Rates(height=1,
+                            rates=[{
+                                'code': 'eur',
+                                'value': 0.5
+                            }, {
+                                'code': 'usd',
+                                'value': 1.0
+                            }])
+
 
 async def get_exchange_rates(test_case):
     result = await test_case.request('/btc/rates/1')
     test_case.assertEqual(rate.to_dict(), result)
+
+    result = await test_case.request('/USDT/rates/1')
+    test_case.assertEqual(rate_usdstable_coin.to_dict(), result)
+
+    result = await test_case.request('/weth/rates/1')
+    test_case.assertEqual(rate_eth.to_dict(), result)
+
+    result = await test_case.request('/eth/rates/1')
+    test_case.assertEqual(rate_eth.to_dict(), result)
