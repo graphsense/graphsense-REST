6230a0ab7402f2be553f11d8e01084c8ffa18ca5
Michael F. <michael.froewis@gmx.at>
2024-12-03 17:52:34 +0100

    fix build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "openapi_server"
-VERSION = "1.9.3-ts2-dev1"
+VERSION = "1.9.0-dev50"
 
 # To install the library, run the following
 #
