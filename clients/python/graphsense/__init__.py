b056019f5546ae62320ba5d68ff470a6c07abaf6
Michael F. <michael.froewis@gmx.at>
2026-01-28 18:03:20 +0100

    update to gslib 2.8.18

--- clients/python/graphsense/__init__.py
+++ clients/python/graphsense/__init__.py
@@ -10,7 +10,7 @@
 """
 
 
-__version__ = "1.17.0rc2"
+__version__ = "1.17.0rc3"
 
 # import ApiClient
 from graphsense.api_client import ApiClient
--- clients/python/graphsense/api_client.py
+++ clients/python/graphsense/api_client.py
@@ -76,7 +76,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/1.17.0rc2/python'
+        self.user_agent = 'OpenAPI-Generator/1.17.0rc3/python'
 
     def __enter__(self):
         return self
